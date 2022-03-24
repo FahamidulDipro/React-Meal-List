6404b0705dc58e1fa1f9641b5ab71fab550d9783
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-03-24 16:50:18 +0600

    Meal details shown

--- src/components/MealLoad/MealLoad.js
+++ src/components/MealLoad/MealLoad.js
@@ -1,19 +1,32 @@
 import React, { useEffect, useState } from 'react';
-
+import Meals from '../Meals/Meals';
+import './MealLoad.css';
 const MealLoad = () => {
     const [meals,setMeal] = useState([]);
+    const [mealDetails,setMealDetails] = useState([]);
     useEffect(()=>{
         fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a').then(res=>res.json()).then(data=>setMeal(data.meals))
     },[])
+    const handleMealDetails = (meal)=>{
+        // console.log(meal);
+        const newMeals = [...meal];
+        setMealDetails(newMeals);
+        
+    }
 
     return (
-        <div>
+        <section className='mealLoadSection'>
+        <div className='AllMealsContainer'>
             {
-                meals.map(meal=>console.log(meal))
-             
-            //   console.log(meals)
+                meals.map(meal=><Meals  key={meal.idMeal} meal={meal} handler={()=>handleMealDetails(meal.strInstructions)}></Meals>)
             }
         </div>
+        <div className='mealDetailsContainer'>
+                <h2>Meal Details</h2>
+                <p>{mealDetails}</p>
+        </div>
+        </section>
+       
     );
 };
 
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import './Meals.css';
 const Meals = (props) => {
+    const {strMeal,strMealThumb,strCategory,strTags,strInstructions,strArea} = props.meal;
+    const {handler} = props;
     return (
-        <div>
-            
+        <div className='MealContainer'>
+         <h2>{strMeal}</h2>
+         <div className='mealImg'>
+             <img src={strMealThumb} alt="mealPic" />
+         </div>
+          <p><b>Catagory: </b>{strCategory}</p>
+          <p><b>Tags: </b>{strTags}</p>
+          <p><b>Area: </b>{strArea}</p>
+          <button className='Btn' onClick={handler}>See Details</button>
+          
         </div>
     );
 };
